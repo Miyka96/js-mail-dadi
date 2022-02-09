654383dad3e0f6c5f1d57cc266cab475065107a5
Miyka96 <micaelaseminara@outlook.it>
2022-02-09 17:20:24 +0100

    first exercise finished

---
+++ app.js
@@ -0,0 +1,29 @@
+const emailInput = document.getElementById('email')
+const emailSubmitButton = document.getElementById('submit')
+
+const emailWhiteList = ["pierino@pierino.it", "gattino@gattino.it", "nonho@fantasia.it", "mipiaccionoitreni@hotmail.it"];
+
+emailSubmitButton.addEventListener('click', function(){
+    const typedEmail = (emailInput.value);
+
+    let check = false;
+
+    for (i=0 ; i< emailWhiteList.length ; i++) {
+        let emailElement = emailWhiteList [i];
+
+        if ( typedEmail===emailElement){
+            check = true;
+        }
+    }
+
+    if (check == true){
+        alert ('top')
+    }
+    else{
+        alert ('no')
+    }
+
+})
+
+
+
